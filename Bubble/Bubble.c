88d7ef05af88564e46344c499a5bcf0dfbec3262
xinke yang <yangdift25@gmail.com>
2026-03-25 19:26:07 +0800

    Create Bubble.c

---
+++ Bubble/Bubble.c
@@ -0,0 +1,16 @@
+//冒泡排序
+void Bubble(int* a, int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (a[j] > a[j + 1])
+			{
+				int tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+}
